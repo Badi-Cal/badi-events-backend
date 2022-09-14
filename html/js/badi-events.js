a28be1cecf7a2e21f1c955cb107cae714ec5396d
J. Basir van de Fliert <jbvandefliert@gmail.com>
2022-09-14 08:48:00 -0400

    Resolved merge conflicts.

--- html/js/badi-events.js
+++ html/js/badi-events.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-const date = Date('2015-03-21');
-=======
 // const date = Date('2015-03-21');
->>>>>>> 166eabfeec2bfe413d9644f82d87f744504822b9
 // const myBadiDate2 = new BadiDate(date);
 // const  datetime = luxon.DateTime.fromISO('2015-03-21');
 //const datetime = luxon.DateTime.fromISO('2020-09-25T19:08:34.123');
@@ -12,11 +8,7 @@ const date = Date('2015-03-21');
 // const myBadiDate1 = new BadiDate(datetime);
 // const date1 = new LocalBadiDate({ year: 2020, month: 1, day: 1 }, -34.6, -58.45, 'America/Argentina/Buenos_Aires');
 // document.getElementById("demo").innerHTML = datetime + "<br>" + myBadiDate1 + "<br>" + date1;
-<<<<<<< HEAD
-document.getElementById("demo").innerHTML = date + "<br>"  + "<br>";
-=======
 // document.getElementById("demo").innerHTML = date + "<br>"  + "<br>";
->>>>>>> 166eabfeec2bfe413d9644f82d87f744504822b9
 // document.getElementById("demo").innerHTML = datetime + "<br>" + date1;
 // document.getElementById("demo").innerHTML = datetime + "<br>" + "<br>";
 
@@ -32,35 +24,6 @@ function setSelectValues(id, values, defaultValue) {
 }
 
 function updateDate() {
-<<<<<<< HEAD
-	var date =
-		new Date(
-			document.getElementById('year').value + '-'
-			+ document.getElementById('month').value + '-'
-			+ document.getElementById('day').value
-		);
-
-	document.getElementById("demo").innerHTML = date + "<br>"  + "<br>";
-}
-
-var yearValues = [];
-for(var i = 1844; i < 2200; i++) {
-	yearValues.push(i);
-}
-setSelectValues('year', yearValues, (new Date()).getFullYear());
-
-var monthValues = [];
-for(var i = 1; i < 13; i++) {
-	monthValues.push(i);
-}
-setSelectValues('month', monthValues, (new Date()).getMonth() + 1);
-
-var dayValues = [];
-for(var i = 1; i < 32; i++) {
-	dayValues.push(i);
-}
-setSelectValues('day', dayValues, (new Date()).getDate()); 
-=======
 	var year = document.getElementById('year').value;
 	var month = document.getElementById('month').value;
 	var day = document.getElementById('day').value;
@@ -122,4 +85,3 @@ function start() {
 
 	updateDate();
 }
->>>>>>> 166eabfeec2bfe413d9644f82d87f744504822b9
