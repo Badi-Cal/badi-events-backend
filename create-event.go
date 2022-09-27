2ce8e6b63d36ccf72a72052063a3e1474e07f2ca
Erik Monti <Erik@Monti.org>
2022-09-27 14:24:36 -0300

    minor advances on create.event

--- create-event.go
+++ create-event.go
@@ -91,6 +91,31 @@ func main() {
         }
 
         t := time.Now().Format(time.RFC3339)
+        event := &calendar.Event{
+            Summary: "Birth of the Báb",
+            Location: "Otamendi 215, Capital Federal, Buenos Aires, Argentina",
+            Description: "The end of the Adamic Cycle.",
+            Start: &calendar.EventDateTime{
+                DateTime: "2022-10-25T19:00:00-03:00",
+                TimeZone: "America/Argentina/Buenos_Aires",
+            },
+            End: &calendar.EventDateTime{
+                DateTime: "2022-10-25T21:00:00-03:00",
+                TimeZone: "America/Argentina/Buenos_Aires",
+            },
+            Attendees: []*calendar.EventAttendee{
+                &calendar.EventAttendee{Email:"erik@monti.org"},
+                &calendar.EventAttendee{Email:"eventos@xn-porteo-0wa.com"},
+            },
+        }
+
+        calendarId := "primary"
+        event, err = srv.Events.Insert(calendarId, event).Do()
+        if err != nil {
+            log.Fatalf("Unable to create event. %v\n", err)
+        }
+        fmt.Printf("Event created: %s\n", event.HtmlLink)
+        // irrelevant stuff I'm leaving in
         events, err := srv.Events.List("primary").ShowDeleted(false).
                 SingleEvents(true).TimeMin(t).MaxResults(10).OrderBy("startTime").Do()
         if err != nil {
@@ -109,32 +134,4 @@ func main() {
                 }
         }
 }
-// Refer to the Go quickstart on how to setup the environment:
-// https://developers.google.com/calendar/quickstart/go
-// Change the scope to calendar.CalendarScope and delete any stored credentials.
 
-event := &calendar.Event{
-  Summary: "Google I/O 2015",
-  Location: "800 Howard St., San Francisco, CA 94103",
-  Description: "A chance to hear more about Google's developer products.",
-  Start: &calendar.EventDateTime{
-    DateTime: "2015-05-28T09:00:00-07:00",
-    TimeZone: "America/Los_Angeles",
-  },
-  End: &calendar.EventDateTime{
-    DateTime: "2015-05-28T17:00:00-07:00",
-    TimeZone: "America/Los_Angeles",
-  },
-  Recurrence: []string{"RRULE:FREQ=DAILY;COUNT=2"},
-  Attendees: []*calendar.EventAttendee{
-    &calendar.EventAttendee{Email:"lpage@example.com"},
-    &calendar.EventAttendee{Email:"sbrin@example.com"},
-  },
-}
-
-calendarId := "primary"
-event, err = srv.Events.Insert(calendarId, event).Do()
-if err != nil {
-  log.Fatalf("Unable to create event. %v\n", err)
-}
-fmt.Printf("Event created: %s\n", event.HtmlLink)
